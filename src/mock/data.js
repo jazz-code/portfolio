91cb6d837c319f6e87ed12bbf61a41b347885ceb
ren.meta@gmail.com <ren.meta@gmail.com>
2020-03-10 17:54:42 -0400

    added WellDone to mockdata, added image

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,9 +2,9 @@ import uuidv1 from 'uuid/v1';
 
 // HEAD DATA
 export const headData = {
-  title: 'Anthony ', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Anthony Rennde', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
+  description: 'Welcome to my portoflio', // e.g: Welcome to my website
 };
 
 // HERO DATA
@@ -21,19 +21,19 @@ export const aboutData = {
   paragraphOne: '',
   paragraphTwo: '',
   paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  resume: 'https://resume.creddle.io/resume/8hh4dzy7tmx', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: uuidv1(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'welldone.png',
+    title: 'WellDone',
+    info: ' - WellDone International, a non-profit working to improve global access to clean water by providing reports on well activity that may facilitate technical assistance to malfunctioning wells. Refactored existing codebase and built onto an existing frontend utilizing React with SemanticUI & EmotionCSS. Key contributor in successful deployment of a data persisting backend utilizing Node, Express, PostgreSQL and Heroku deployment.',
+    info2: 'Login/Passowrd: admin@email / password (Super secure! Please allow 30 seconds for database to fire up from its sleeping state)',
+    url: 'https://well-done-h9vshme77.now.sh',
+    repo: 'https://github.com/labs17-WellDone/backend', // if no repo, the button will not show up
   },
   {
     id: uuidv1(),
