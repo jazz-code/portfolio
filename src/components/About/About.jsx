0d12e59a26a94f06ff8c608310f01ba477af9320
Anthony Rende <ren.meta@gmail.com>
2020-09-06 12:42:49 -0400

    edited iframe for game of life && minor styling changes on display

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,15 +1,15 @@
 import React, { useContext, useState, useEffect } from 'react';
 import Fade from 'react-reveal/Fade';
 import { Container, Row, Col } from 'react-bootstrap';
+import Iframe from 'react-iframe';
 import Title from '../Title/Title';
-import AboutImg from '../Image/AboutImg';
+// import AboutImg from '../Image/AboutImg';
 import PortfolioContext from '../../context/context';
-import Iframe from 'react-iframe'
 
 const About = () => {
   const { about } = useContext(PortfolioContext);
-  const { img, paragraphOne, paragraphTwo, paragraphThree, paragraphFour, resume } = about;
-
+  const { paragraphOne, paragraphTwo, paragraphThree, paragraphFour, resume } = about;
+  // ^ add in:img,
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
 
@@ -31,7 +31,11 @@ const About = () => {
           <Col md={6} sm={12}>
             <Fade bottom duration={1000} delay={600} distance="30px">
               <div className="about-wrapper__image">
-              <Iframe id='ifr' width='540' height='395' scrolling='no' style='background: url(//studio.processingtogether.com/static/img/jun09/pad/connectingbar.gif) no-repeat center 60px;' src='//studio.processingtogether.com/sp/pad/iframe/ro.9DMuq9xr7ds7I/rev.11?autostart=1'/>
+                <Iframe
+                  src="https://www.openprocessing.org/sketch/955851/embed/"
+                  width="650"
+                  height="470"
+                />
               </div>
             </Fade>
           </Col>
@@ -39,17 +43,20 @@ const About = () => {
             <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
               <div className="about-wrapper__info">
                 <p className="about-wrapper__info-text">
-                  {paragraphOne || "I'm a Software Developer with experience on both ends of the stack, focused on providing meaningful, genuine contributions to colleagues, stakeholders, and end-users"}
+                  {paragraphOne ||
+                    "I'm a Software Developer with experience on both ends of the stack, focused on providing meaningful, genuine contributions to colleagues, stakeholders, and end-users"}
                 </p>
                 <p className="about-wrapper__info-text">
                   {paragraphTwo ||
                     'Experienced in building products in a team setting with daily collaboration in software development subjects'}
                 </p>
                 <p className="about-wrapper__info-skills">
-                  {paragraphThree || 'My main skills are JavaScript, ES6+, React, HTML/CSS, Node/Express, REST, Python, SQL, PostgreSQL, Jest, Linux '}
+                  {paragraphThree ||
+                    'My main skills are JavaScript, ES6+, React, HTML/CSS, Node/Express, REST, Python, SQL, PostgreSQL, Jest, Linux '}
                 </p>
                 <p className="about-wrapper__info-text">
-                  {paragraphFour || "When I’m not coding-- I love to consume sci-fi, sharpen my guitar skills, or experience new places around NYC and indulge my love for live music"}
+                  {paragraphFour ||
+                    'When I’m not coding-- I love to consume sci-fi, sharpen my guitar skills, or experience new places around NYC and indulge my love for live music'}
                 </p>
                 {resume && (
                   <span className="d-flex mt-3">
